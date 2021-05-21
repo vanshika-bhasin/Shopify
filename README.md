7d50c2ac1e953bd57ad36c3d760c09fe19b201c6
Vanshika <vanshikabhasin9@gmail.com>
2021-05-21 17:40:36 +0530

    Files added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Shopify
+
+An e-commerce website to buy a variety of products.
---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p style="font-size:16px;" align="center">Copyright © Shopify by VANSHIKA BHASIN. All Rights Reserved | Contact Us: +2210005522</p>
+    </div>
+</footer>
